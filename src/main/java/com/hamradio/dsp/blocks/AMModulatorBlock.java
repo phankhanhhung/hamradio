68afa6b96fea0bced05b296bbcf8e5405f5a4ce9
Nhat Minh Phan <nhatminh.phan.2k4@gmail.com>
2026-02-19 08:44:35 +0700

    Implement AM modulator and demodulator blocks

---
+++ src/main/java/com/hamradio/dsp/blocks/AMModulatorBlock.java
@@ -0,0 +1,29 @@
+package com.hamradio.dsp.blocks;
+
+import com.hamradio.dsp.NativeDSP;
+
+public class AMModulatorBlock extends ProcessingBlock {
+
+    private final NativeDSP dsp = new NativeDSP();
+    private final float carrierFreq;
+    private final int sampleRate;
+
+    public AMModulatorBlock(String id, float carrierFreq, int sampleRate) {
+        super(id);
+        this.carrierFreq = carrierFreq;
+        this.sampleRate = sampleRate;
+    }
+
+    @Override
+    public void process(float[] input, float[] output, int numSamples) {
+        float[] result = dsp.modulateAM(input, carrierFreq, sampleRate);
+        if (result != null) {
+            System.arraycopy(result, 0, output, 0, Math.min(result.length, output.length));
+        }
+    }
+
+    @Override
+    public int getOutputSize(int inputSize) {
+        return inputSize;
+    }
+}
